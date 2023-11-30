dad0b1d4ff8344e8afa3bb06b56688e0f3121b2b
Ratul Hasan <34002411+RaSan147@users.noreply.github.com>
2023-12-01 02:45:26 +0600

    Fixed algorithm (was generating next line > Fixed)

--- Pascal Triangle/pascal triangle.py
+++ Pascal Triangle/pascal triangle.py
@@ -4,31 +4,48 @@ for i in range(1,13,2):
 line= int(input('line num: '))
 base=[0,1,0]
 
-if line<=0:
+def intLen(n):
+	i = 0
+	j = 1
+	while n >= j:
+		i += 1
+		j *= 10
+	return i
+
+
+def printLine(base, distance):
+	s=''
+
+	for n, x in enumerate(base):
+		if x==0:
+			continue
+		
+		if intLen(x)>5 and n != 0:
+			space='|'
+		else:
+			space=' '*(6-intLen(x))
+		s+=str(x)
+		if base.index(x)<len(base)-1:
+			s+=space
+
+	print(s.center(distance))
+
+if line<1:
 	print("can't do that")
 else:
-	for i in range(line):
+	distance=line+10*(line-1)
+	printLine(base, distance)
+	for i in range(1, line):
 		distance=line+10*(line-1)
-		# print(distance)
+
 		new_base=[0,]
-		s=''
 		ind=0
 		for x in base[:len(base)-1]:
 			new_base.append(x+base[ind+1])
-			if x==0:
-				pass
-			else:
-				if len(str(x))>5:
-					space='|'
-				else:
-					space=' '*(6-len(str(x)))
-				s+=str(x)
-				if base.index(x)<len(base)-1:
-					s+=space
+
 			ind+=1
-		#print(base[7])
-		# print(new_base)
-		print(s.center(distance))
+
 		new_base.append(0)
 		base=new_base
-		#print(base)
+
+		printLine(new_base, distance)
